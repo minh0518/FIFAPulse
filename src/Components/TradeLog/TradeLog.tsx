969fb06de740d537f45e5471a24a33b8c93db78a
minh0518 <rubics@naver.com>
2023-06-02 15:25:33 +0900

    :sparkles: [#112][Feat] : 이적시장 목록 컴포넌트 뼈대 구현
    내 기록 페이지(MyRecord.tsx)에 사용되는 이적시장 목록 컴포넌트(TradeLog.tsx)의 뼈대를 구현하고 관련 데이터들이 제대로 렌더링 되는지 확인합니다

--- src/Components/TradeLog/TradeLog.tsx
+++ src/Components/TradeLog/TradeLog.tsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TradeLogContainerDiv } from './TradeLog.styled';
+import { useUserObjAPI } from '../../Context/UserObj/UserObjContext';
+import FIFAData from '../../Services/FifaData';
+import { TradeLogInfo } from '../../types/api';
+import { addCommaonMoney, convertDateAndTime } from '../../utils/MyRecord';
+import MatchResultsByMatchTypes from '../MatchResultsByMatchTypes';
+import PlayerImg from '../MatchStatistics/Player/PlayerImg';
 
 const TradeLog = () => {
-  return <TradeLogContainerDiv>이적시장 목록</TradeLogContainerDiv>;
+  const { userObj, setUserObj } = useUserObjAPI()!;
+
+  const [tradeInfo, setTradeInfo] = useState<{ buy: TradeLogInfo[]; sell: TradeLogInfo[] } | null>(null);
+  const [tradeType, setTradeType] = useState<'buy' | 'sell'>('buy');
+
+  // console.log(tradeInfo && tradeInfo[tradeType]);
+
+  useEffect(() => {
+    const test = async () => {
+      const fifa = new FIFAData();
+      const result = await fifa.getTradeLog(userObj!.FIFAOnlineAccessId);
+      setTradeInfo(result);
+    };
+    test();
+  }, []);
+
+  return (
+    <TradeLogContainerDiv>
+      <h2>이적 시장 목록</h2>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>BUY</th>
+              <th>SELL</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+
+      {tradeInfo && (
+        <div>
+          <table>
+            <tbody>
+              {tradeInfo[tradeType].map((i) => {
+                return (
+                  <tr key={i.saleSn}>
+                    {/* PlayerImg 폴더 위치 변경해야 할듯 */}
+                    <PlayerImg spId={i.spid} />
+                    <td>+{i.grade}</td>
+                    <td>{convertDateAndTime(i.tradeDate)}</td>
+                    <td>{addCommaonMoney(i.value)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </TradeLogContainerDiv>
+  );
 };
 
 export default TradeLog;
