f54ec909b6a5b03ea9b198a2271942c8fc1c3028
minh0518 <rubics@naver.com>
2023-06-30 16:15:05 +0900

    :sparkles: [#166][Feat] : Pass 컴포넌트 apex chart 적용
    패스 관련 통계를 보여주는 Pass 컴포넌트에 apex chart를 통한 시각화 기능을 구현하고 , 사용된 apex chart에 반응형 스타일을 적용합니다

--- src/Components/Statistics/Pass/Pass.styled.ts
+++ src/Components/Statistics/Pass/Pass.styled.ts
@@ -1,9 +1,30 @@
+import ReactApexChart from 'react-apexcharts';
 import styled from 'styled-components';
 
 export const PassContainerDiv = styled.div`
   margin: 50px 0;
   display: flex;
   justify-content: space-around;
+  @media (max-width: 1024px) {
+    flex-direction: column;
+  }
 `;
 
-export const tmp = '';
+export const PassChartDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: 1024px) {
+    margin: 25px 0;
+  }
+`;
+
+export const StyledChart = styled(ReactApexChart)`
+  width: 600px;
+  margin: 0 30px;
+
+  @media (max-width: 1420px) {
+    //1024가 되어서  flex-direction: column;이 되어도 width를 계속 100% 사용
+    width: 100%;
+  }
+`;
