b638dfbef735c1a0d60a25bd8df40a91b0b54db7
minh0518 <rubics@naver.com>
2023-07-02 13:56:45 +0900

    :recycle: [#170][Refactor] : 배포 오류 수정(export)
    배포를 위해 사용하지 않고 있었던 SoccerGround 컴포넌트를 빈 컴포넌트 형태로 export 합니다

--- src/Components/Statistics/Shoot/SoccerGround/SoccerGround.tsx
+++ src/Components/Statistics/Shoot/SoccerGround/SoccerGround.tsx
@@ -1,7 +1,7 @@
-// import React from 'react';
+import React from 'react';
 
-// const SoccerGround = ({}) => {
-//   return <div>sdfsd</div>;
-// };
+const SoccerGround = () => {
+  return <div>SoccerGround</div>;
+};
 
-// export default SoccerGround;
+export default SoccerGround;
