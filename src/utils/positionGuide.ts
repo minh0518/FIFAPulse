1d3a804e1d53e546fbd639bea55a0f70d1f771ff
minh0518 <rubics@naver.com>
2023-08-05 01:39:19 +0900

    :sparkles: [#231][Feat] : 포지션 통계 페이지 최대 수치 조건부 스타일 적용
    포지션 통계 페이지 PositionGuide.tsx에 선택된 포지션들에 해당하는 수치 중, 각 분야별로 가장 높은 수치에 대해 조건부로 글자 색상을 다르게 보여주는 기능을 구현합니다

--- src/utils/positionGuide.ts
+++ src/utils/positionGuide.ts
@@ -7,3 +7,59 @@ export const getStringMatchResults = (inputValue: string, dropDownList: any) => 
   return result;
 };
 export const tmp = '';
+
+export const getMaxInfo = (rankerInfo: any) => {
+  const maxInfo = {
+    shoot: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.shoot - a[0].status.shoot;
+      })[0][0].status.shoot,
+    ),
+    effectiveShoot: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.effectiveShoot - a[0].status.effectiveShoot;
+      })[0][0].status.effectiveShoot,
+    ),
+    assist: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.assist - a[0].status.assist;
+      })[0][0].status.assist,
+    ),
+    goal: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.goal - a[0].status.goal;
+      })[0][0].status.goal,
+    ),
+    dribbleTry: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.dribbleTry - a[0].status.dribbleTry;
+      })[0][0].status.dribbleTry,
+    ),
+    dribbleSuccess: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.dribbleSuccess - a[0].status.dribbleSuccess;
+      })[0][0].status.dribbleSuccess,
+    ),
+    passTry: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.passTry - a[0].status.passTry;
+      })[0][0].status.passTry,
+    ),
+    passSuccess: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.passSuccess - a[0].status.passSuccess;
+      })[0][0].status.passSuccess,
+    ),
+    block: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.block - a[0].status.block;
+      })[0][0].status.block,
+    ),
+    tackle: Number(
+      [...rankerInfo].sort((a, b) => {
+        return b[0].status.tackle - a[0].status.tackle;
+      })[0][0].status.tackle,
+    ),
+  };
+  return maxInfo;
+};
