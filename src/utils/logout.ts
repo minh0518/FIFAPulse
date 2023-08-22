a7db7c4e0f0d123ac5cec1bb725fbb213a54efba
minh0518 <rubics@naver.com>
2023-08-22 19:56:04 +0900

    :recycle: [#240][Refactor] : 로그인 기능 onAuthStateChanged 제거
    onAuthStateChanged 콜백 안에 모든 로그인 로직이 절차식으로 복잡하게 진행되던 기존 로그인 로직을 함수 단위로 분리하며 더이상 onAuthStateChanged에 의존하지 않습니다

---
+++ src/utils/logout.ts
@@ -0,0 +1,19 @@
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { authService } from '../../firebase';
+
+const onLogoutClick = async (setIsNickNameExist: any, setUserObj: any, setIsNicknameChanged: any, navigate: any) => {
+  setIsNickNameExist(null); // 모달 창에서 뒤로가기 선택하고 재 로그인시
+  // 모달창 띄우는 useEffect를 실행하기 위해서 의존성 배열을 변경해야 하므로
+  // 의도적으로 isNickNameExist를 초기값으로 세팅
+  // (로그아웃 됐을 때 null -> 재로그인시 false로 변경돼서 useEffect 실행)
+
+  setUserObj(null);
+  localStorage.setItem('userObj', JSON.stringify(null));
+
+  setIsNicknameChanged(false);
+  localStorage.setItem('isNicknameChanged', JSON.stringify(false));
+
+  signOut(authService);
+  navigate('/', { replace: true });
+};
